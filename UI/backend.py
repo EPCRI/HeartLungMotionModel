688f47632a81378db67f62743886ffeb2ac43a1a
Wendytanj <jt58@rice.edu>
2024-07-17 19:07:38 -0500

    daily

---
+++ UI/backend.py
@@ -0,0 +1,112 @@
+
+from frontend import Ui_MainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QButtonGroup, QComboBox, QLineEdit, QLabel, QDialog
+from PyQt5.QtGui import QIcon
+from PyQt5 import QtCore, QtWidgets
+import sys
+import time
+import serial
+from serial.tools import list_ports
+import numpy as np
+import pyqtgraph as pg
+from random import randint
+# import matplotlib.pyplot as plt
+
+
+class Gooey(QMainWindow, Ui_MainWindow): 
+    def __init__(self, parent=None):
+        super(Gooey, self).__init__(parent)
+        self.setupUi(self)
+        self.buttonStart.clicked.connect(self.Start)
+        self.buttonStop.clicked.connect(self.Stop)
+        # self.Calibrate_pushButton.clicked.connect(self.Calibrate)
+        # self.Hard_Stop_pushButton.clicked.connect(self.Hard_Stop)
+        self.buttonRefresh.clicked.connect(self.Refresh)
+        # self.Connect_pushButton.clicked.connect(self.Connect)
+        self.buttonWaveFormTest.clicked.connect(self.Wave)
+
+        self.labelConnectionStatus.setText("Connect to serial port.")
+
+    def Start(self): # Start button to run motors 
+        self.ser.write('P'.encode()) 
+        Start = self.ser.read()
+        print(Start)
+    
+    def Stop(self): # No stop button code on arduino 
+        self.ser.write(''.encode())
+        Stop = self.ser.read()
+        print(Stop)
+
+    def Refresh(self): # Refresh button to intake new COMs 
+        self.comboBoxComPort.clear()
+        list_of_ports = serial.tools.list_ports.comports()
+        print (list_of_ports)
+        for port in list_of_ports:
+            self.comboBoxComPort.addItem(port.name)
+
+    def Connect(self): #Connect button for connecting to COMs 
+        portname = self.comboBoxComPort.currentText()
+        self.ser = serial.Serial(portname, baudrate=9600, timeout=1, write_timeout=1)
+        time.sleep(2)
+        self.ser.write('T'.encode())
+        Itemscheck = self.ser.read()
+        print(portname)
+        print(Itemscheck)
+        if Itemscheck == (b'C'):
+            print ("COM connected")
+        else:   
+            print ("COM not connected")
+
+    def Calibrate(self): #Calibrate button for restarting the motor start point 
+        self.ser.write('C'.encode())
+        Calibrate = self.ser.read()
+        print(Calibrate)
+        if Calibrate == (b'Y'):
+            print("Calibrating")
+    
+    def Hard_Stop(self): #Hard stop button for if calibration goes wrong 
+        self.ser.write('X'.encode())
+        Hard_Stop = self.ser.read()
+        print(Hard_Stop)
+        if Hard_Stop == (b'Y'):
+            print("Stop Calibrating")
+        
+
+    def Wave(self):
+        self.widgetWaveForm.clear()
+        self.heart_amplitude = float(self.lineEditHeartAmp.text())
+        print("Heart amplitude reading", self.heart_amplitude)
+        self.heart_frequency = float(self.lineEditHeartFreq.text())
+        self.lung_amplitude = float(self.lineEditLungAmp.text())
+        self.lung_frequency = float(self.lineEditLungFreq.text())
+        self.Phase_Diff = float(self.lineEditPhaseDiff.text())
+        dt = 0.05
+        wave_period = 1 / self.lung_frequency
+        print("Period: ", wave_period)
+        t = np.arange(0, wave_period, dt)
+        heart_pen = pg.mkPen(color=(255,0,0), width=3)
+        heart_motion = self.heart_amplitude * np.sin(2 * np.pi * self.heart_frequency * t) # (self.Phase_Diff/360 * 2 * np.pi)
+        self.widgetWaveForm.addLegend()
+        self.widgetWaveForm.plot(t, heart_motion,  pen=heart_pen, label='Heart Motion', name="Heart Motion")
+        lung_pen = pg.mkPen(color=(0,255,0), width=3)
+        lung_motion = self.lung_amplitude * np.sin(2 * np.pi * self.lung_frequency * t)
+        self.widgetWaveForm.plot(t, lung_motion,  pen=lung_pen, label='Lung Motion', name="Lung Motion")
+        combined_pen = pg.mkPen(color=(0,0,255), width=3)
+        combined_motion = heart_motion + lung_motion
+        self.widgetWaveForm.plot(t, combined_motion,  pen=combined_pen, label='Combined Motion', linestyle='--', name="Combined motion")
+        self.widgetWaveForm.setXRange(0, wave_period)
+        return heart_motion, lung_motion, combined_motion
+ 
+     
+     
+def main():
+    app = QApplication(sys.argv)
+    app.setStyle('fusion')
+    form = Gooey()
+    form.show()
+    sys.exit(app.exec())
+
+
+if __name__ == "__main__":
+    main()
+
---
+++ UI/frontend.py
@@ -0,0 +1,240 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+import pyqtgraph as pg
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(1000, 800)  # Window size
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+
+        # Main layout
+        self.mainLayout = QtWidgets.QVBoxLayout(self.centralwidget)
+
+        # Groupbox setup
+        self.groupBoxSetup = QtWidgets.QGroupBox(self.centralwidget)
+        font = QtGui.QFont()
+        font.setFamily("Arial")
+        font.setPointSize(11)
+        self.groupBoxSetup.setFont(font)
+        self.groupBoxSetup.setObjectName("groupBoxSetup")
+
+        self.setupLayout = QtWidgets.QHBoxLayout(self.groupBoxSetup)
+        self.labelComPort = QtWidgets.QLabel(self.groupBoxSetup)
+        self.labelComPort.setFont(font)
+        self.labelComPort.setObjectName("labelComPort")
+        self.setupLayout.addWidget(self.labelComPort)
+
+        self.comboBoxComPort = QtWidgets.QComboBox(self.groupBoxSetup)
+        self.comboBoxComPort.setFont(font)
+        self.comboBoxComPort.setObjectName("comboBoxComPort")
+        self.setupLayout.addWidget(self.comboBoxComPort)
+
+        self.buttonRefresh = QtWidgets.QPushButton(self.groupBoxSetup)
+        self.buttonRefresh.setFont(font)
+        self.buttonRefresh.setObjectName("buttonRefresh")
+        self.setupLayout.addWidget(self.buttonRefresh)
+
+        self.labelConnectionStatus = QtWidgets.QLabel(self.groupBoxSetup)
+        self.labelConnectionStatus.setFont(font)
+        self.labelConnectionStatus.setObjectName("labelConnectionStatus")
+        self.setupLayout.addWidget(self.labelConnectionStatus)
+
+        self.mainLayout.addWidget(self.groupBoxSetup)
+
+        # Calibrate group box
+        self.groupBoxCalibrate = QtWidgets.QGroupBox(self.centralwidget)
+        self.groupBoxCalibrate.setFont(font)
+        self.groupBoxCalibrate.setObjectName("groupBoxCalibrate")
+
+        self.calibrateLayout = QtWidgets.QHBoxLayout(self.groupBoxCalibrate)
+        self.labelDisplacement = QtWidgets.QLabel(self.groupBoxCalibrate)
+        self.labelDisplacement.setFont(font)
+        self.labelDisplacement.setObjectName("labelDisplacement")
+        self.calibrateLayout.addWidget(self.labelDisplacement)
+
+        self.lineEditDisplacement = QtWidgets.QLineEdit(self.groupBoxCalibrate)
+        self.lineEditDisplacement.setFont(font)
+        self.lineEditDisplacement.setObjectName("lineEditDisplacement")
+        self.calibrateLayout.addWidget(self.lineEditDisplacement)
+
+        self.buttonUp = QtWidgets.QPushButton(self.groupBoxCalibrate)
+        self.buttonUp.setFont(font)
+        self.buttonUp.setFixedSize(220, 40)
+        self.buttonUp.setObjectName("buttonUp")
+        self.calibrateLayout.addWidget(self.buttonUp)
+
+        self.buttonDown = QtWidgets.QPushButton(self.groupBoxCalibrate)
+        self.buttonDown.setFont(font)
+        self.buttonDown.setFixedSize(220, 40)
+        self.buttonDown.setObjectName("buttonDown")
+        self.calibrateLayout.addWidget(self.buttonDown)
+
+        self.mainLayout.addWidget(self.groupBoxCalibrate)
+
+        # Parameters group box
+        self.groupBoxParameters = QtWidgets.QGroupBox(self.centralwidget)
+        self.groupBoxParameters.setFont(font)
+        self.groupBoxParameters.setObjectName("groupBoxParameters")
+
+        self.parametersLayout = QtWidgets.QGridLayout(self.groupBoxParameters)
+
+        self.labelHeartText = QtWidgets.QLabel(self.groupBoxParameters)
+        self.labelHeartText.setFont(font)
+        self.labelHeartText.setObjectName("labelHeartText")
+        self.parametersLayout.addWidget(self.labelHeartText, 0, 0)
+
+        self.labelAmpHeart = QtWidgets.QLabel(self.groupBoxParameters)
+        self.labelAmpHeart.setFont(font)
+        self.labelAmpHeart.setObjectName("labelAmpHeart")
+        self.labelAmpHeart.setAlignment(QtCore.Qt.AlignRight)
+        self.parametersLayout.addWidget(self.labelAmpHeart, 0, 1)
+
+        self.lineEditHeartAmp = QtWidgets.QLineEdit(self.groupBoxParameters)
+        self.lineEditHeartAmp.setFont(font)
+        self.lineEditHeartAmp.setObjectName("lineEditHeartAmp")
+        self.parametersLayout.addWidget(self.lineEditHeartAmp, 0, 2)
+
+        self.labelFreqHeart = QtWidgets.QLabel(self.groupBoxParameters)
+        self.labelFreqHeart.setFont(font)
+        self.labelFreqHeart.setObjectName("labelFreqHeart")
+        self.labelFreqHeart.setAlignment(QtCore.Qt.AlignRight)
+        self.parametersLayout.addWidget(self.labelFreqHeart, 0, 3)
+
+        self.lineEditHeartFreq = QtWidgets.QLineEdit(self.groupBoxParameters)
+        self.lineEditHeartFreq.setFont(font)
+        self.lineEditHeartFreq.setObjectName("lineEditHeartFreq")
+        self.parametersLayout.addWidget(self.lineEditHeartFreq, 0, 4)
+
+        self.labelHzHeart = QtWidgets.QLabel(self.groupBoxParameters)
+        self.labelHzHeart.setFont(font)
+        self.labelHzHeart.setObjectName("labelHzHeart")
+        self.parametersLayout.addWidget(self.labelHzHeart, 0, 5)
+
+        self.labelLungText = QtWidgets.QLabel(self.groupBoxParameters)
+        self.labelLungText.setFont(font)
+        self.labelLungText.setObjectName("labelLungText")
+        self.parametersLayout.addWidget(self.labelLungText, 1, 0)
+
+        self.labelAmpLung = QtWidgets.QLabel(self.groupBoxParameters)
+        self.labelAmpLung.setFont(font)
+        self.labelAmpLung.setObjectName("labelAmpLung")
+        self.labelAmpLung.setAlignment(QtCore.Qt.AlignRight)
+        self.parametersLayout.addWidget(self.labelAmpLung, 1, 1)
+
+        self.lineEditLungAmp = QtWidgets.QLineEdit(self.groupBoxParameters)
+        self.lineEditLungAmp.setFont(font)
+        self.lineEditLungAmp.setObjectName("lineEditLungAmp")
+        self.parametersLayout.addWidget(self.lineEditLungAmp, 1, 2)
+
+        self.labelFreqLung = QtWidgets.QLabel(self.groupBoxParameters)
+        self.labelFreqLung.setFont(font)
+        self.labelFreqLung.setObjectName("labelFreqLung")
+        self.labelFreqLung.setAlignment(QtCore.Qt.AlignRight)
+        self.parametersLayout.addWidget(self.labelFreqLung, 1, 3)
+
+        self.lineEditLungFreq = QtWidgets.QLineEdit(self.groupBoxParameters)
+        self.lineEditLungFreq.setFont(font)
+        self.lineEditLungFreq.setObjectName("lineEditLungFreq")
+        self.parametersLayout.addWidget(self.lineEditLungFreq, 1, 4)
+
+        self.labelHzLung = QtWidgets.QLabel(self.groupBoxParameters)
+        self.labelHzLung.setFont(font)
+        self.labelHzLung.setObjectName("labelHzLung")
+        self.parametersLayout.addWidget(self.labelHzLung, 1, 5)
+
+        self.labelDistRevol = QtWidgets.QLabel(self.groupBoxParameters)
+        self.labelDistRevol.setFont(font)
+        self.labelDistRevol.setObjectName("labelDistRevol")
+        self.parametersLayout.addWidget(self.labelDistRevol, 2, 0)
+
+        self.lineEditDistRevol = QtWidgets.QLineEdit(self.groupBoxParameters)
+        self.lineEditDistRevol.setFont(font)
+        self.lineEditDistRevol.setObjectName("lineEditDistRevol")
+        self.parametersLayout.addWidget(self.lineEditDistRevol, 2, 1, 1, 2)  # Span multiple columns
+
+        self.labelMaxAmp = QtWidgets.QLabel(self.groupBoxParameters)
+        self.labelMaxAmp.setFont(font)
+        self.labelMaxAmp.setObjectName("labelMaxAmp")
+        self.labelMaxAmp.setAlignment(QtCore.Qt.AlignRight)
+        self.parametersLayout.addWidget(self.labelMaxAmp, 2, 4)
+
+        self.mainLayout.addWidget(self.groupBoxParameters)
+
+
+        self.buttonLayout = QtWidgets.QHBoxLayout()
+
+        self.buttonStart = QtWidgets.QPushButton(self.centralwidget)
+        self.buttonStart.setFont(font)
+        self.buttonStart.setObjectName("buttonStart")
+        self.buttonLayout.addWidget(self.buttonStart)
+
+        self.buttonStop = QtWidgets.QPushButton(self.centralwidget)
+        self.buttonStop.setFont(font)
+        self.buttonStop.setObjectName("buttonStop")
+        self.buttonLayout.addWidget(self.buttonStop)
+    
+        self.buttonWaveFormTest = QtWidgets.QPushButton(self.centralwidget)
+        self.buttonWaveFormTest.setFont(font)
+        self.buttonWaveFormTest.setObjectName("buttonWaveFormTest")
+        self.buttonLayout.addWidget(self.buttonWaveFormTest)
+
+        self.mainLayout.addLayout(self.buttonLayout)
+
+        # Waveform group box
+        self.groupBoxWaveForm = QtWidgets.QGroupBox(self.centralwidget)
+        self.groupBoxWaveForm.setFont(font)
+        self.groupBoxWaveForm.setObjectName("groupBoxWaveForm")
+        self.groupBoxWaveForm.setMinimumHeight(350)  # Adjust the height as needed
+        self.waveformLayout = QtWidgets.QVBoxLayout(self.groupBoxWaveForm)
+
+        self.widgetWaveForm = pg.PlotWidget(self.groupBoxWaveForm)
+        styles = {"color": "black", "font-size": "15px"}
+        self.widgetWaveForm.setLabel("left", "Amp HZ", **styles)
+        self.widgetWaveForm.setLabel("bottom", "Freq HZ", **styles)
+        self.widgetWaveForm.setTitle("Combined Heart Lung Motion", color="k", size="12pt")
+        self.widgetWaveForm.setBackground("w")
+        self.waveformLayout.addWidget(self.widgetWaveForm)
+
+        self.mainLayout.addWidget(self.groupBoxWaveForm, stretch=1)  # Stretch to adjust size with window
+
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 1000, 21))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.groupBoxSetup.setTitle(_translate("MainWindow", "Setup"))
+        self.buttonRefresh.setText(_translate("MainWindow", "Refresh"))
+        self.labelComPort.setText(_translate("MainWindow", "COM Port:"))
+        self.labelConnectionStatus.setText(_translate("MainWindow", "Arduino connected, etc."))
+        self.groupBoxCalibrate.setTitle(_translate("MainWindow", "Calibrate"))
+        self.labelDisplacement.setText(_translate("MainWindow", "Displacement (cm):"))
+        self.buttonUp.setText(_translate("MainWindow", "Up"))
+        self.buttonDown.setText(_translate("MainWindow", "Down"))
+        self.groupBoxParameters.setTitle(_translate("MainWindow", "Parameters"))
+        self.labelHeartText.setText(_translate("MainWindow", "Heart Motion"))
+        self.labelAmpHeart.setText(_translate("MainWindow", "Amp"))
+        self.labelFreqHeart.setText(_translate("MainWindow", "Freq"))
+        self.labelHzHeart.setText(_translate("MainWindow", "Hz"))
+        self.labelLungText.setText(_translate("MainWindow", "Lung Motion"))
+        self.labelAmpLung.setText(_translate("MainWindow", "Amp"))
+        self.labelFreqLung.setText(_translate("MainWindow", "Freq"))
+        self.labelHzLung.setText(_translate("MainWindow", "Hz"))
+        self.buttonStart.setText(_translate("MainWindow", "Start"))
+        self.buttonStop.setText(_translate("MainWindow", "Stop"))
+        self.buttonWaveFormTest.setText(_translate("MainWindow", "Waveform Test"))
+        self.labelDistRevol.setText(_translate("MainWindow", "Distance per rev. (mm)"))
+        self.labelMaxAmp.setText(_translate("MainWindow", "Max Amplitude: x cm"))
+        self.groupBoxWaveForm.setTitle(_translate("MainWindow", "Waveform"))
+
+
